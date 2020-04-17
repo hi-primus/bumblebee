237ff1f098b242cf63a5b7c0089865beffa84248
Luis <luisaguirre027@gmail.com>
2020-04-17 17:28:19 -0400

    Removed pako and fernet function

--- api/utils/functions.js
+++ api/utils/functions.js
@@ -1,25 +1,7 @@
-const fernet = require('fernet')
-const pako = require('pako')
-
 export const trimCharacters = (s, c) => {
   if (c === "]") c = "\\]";
   if (c === "\\") c = "\\\\";
   return s.replace(new RegExp(
     "^[" + c + "]+|[" + c + "]+$", "g"
   ), "");
-}
-
-export const pakoFernet = (secret, content) => {
-  let token = new fernet.Token({
-    secret: new fernet.Secret(secret),
-    token: content,
-    ttl: 0
-  })
-
-  let data = pako.inflate(
-    Buffer.from(token.decode(),'base64').toString('binary'),
-    { to: 'string' }
-  )
-
-  return JSON.parse(data)
 }
