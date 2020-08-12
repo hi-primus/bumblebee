f1f65adf8bd3c63bfcfc88bed541d8870c8f054e
Eduardo Gonzalez <eduardgp@gmail.com>
2020-08-12 19:10:04 +0200

    [FEAT] Cron job for backend tasks

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,16 +2,19 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { CronService } from "./cron.service";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
 import { DatasourceModule } from "./datasource/datasource.module";
 import { WorkspaceModule } from "./workspace/workspace.module";
 import { AppGateway } from "./app.gateway";
 import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     MongooseModule.forRoot(
       "mongodb+srv://bumblebee:6oF5B3omteLRPOQl@cluster0-9mqzj.mongodb.net/bumblebee?retryWrites=true&w=majority",
       {
@@ -26,6 +29,6 @@ import { ConfigModule } from "@nestjs/config";
     DatasourceModule,
   ],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService, CronService, AppGateway],
 })
 export class AppModule {}
---
+++ packages/api/src/cron.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
+
+@Injectable()
+export class CronService {
+  private readonly logger = new Logger(CronService.name);
+
+  @Cron("* * */12 * * *")
+  handleCron() {
+    this.logger.debug("Called when the current second is multiple of 5");
+  }
+}
