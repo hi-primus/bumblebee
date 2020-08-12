c956f609ebfd1043770e0dcd57a7cf4798ee1c4e
Eduardo Gonzalez <eduardgp@gmail.com>
2020-08-12 17:55:23 +0200

    [FEAT] Added new function to delete kernels on init
    - [FIX] Changed datasource requirements on upload

--- packages/api/src/datasource/datasource.service.ts
+++ packages/api/src/datasource/datasource.service.ts
@@ -1,82 +1,82 @@
-import { Model } from 'mongoose';
-import { Injectable, BadRequestException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { DataSource } from './interfaces/datasoruce.interface';
-import { User } from 'src/users/interfaces/user.interface';
-import { v4 as uuidv4 } from 'uuid';
-import * as AWS from 'aws-sdk';
+import { Model } from "mongoose";
+import { Injectable, BadRequestException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { DataSource } from "./interfaces/datasoruce.interface";
+import { User } from "src/users/interfaces/user.interface";
+import { v4 as uuidv4 } from "uuid";
+import * as AWS from "aws-sdk";
 
 @Injectable()
 export class DatasourceService {
-	constructor(
-		@InjectModel('DataSource') private dataSourceModel: Model<DataSource>,
-	) {}
-	s3 = new AWS.S3({
-		endpoint: process.env.DO_ENDPOINT,
-		accessKeyId: process.env.DO_ACCESS_KEY_ID,
-		secretAccessKey: process.env.DO_SECRET_KEY,
-	});
+  constructor(
+    @InjectModel("DataSource") private dataSourceModel: Model<DataSource>
+  ) {}
+  s3 = new AWS.S3({
+    endpoint: process.env.DO_ENDPOINT,
+    accessKeyId: process.env.DO_ACCESS_KEY_ID,
+    secretAccessKey: process.env.DO_SECRET_KEY,
+  });
 
-	async getPresignedUrl(user, media): Promise<string> {
-		const name = media.name.split('.').slice(0, -1).join('.');
-		const Key = `${user.username}/${name}-${uuidv4()}.${
-			media.type.split('/')[1]
-		}`;
-		try {
-			const url = await this.s3.getSignedUrlPromise('putObject', {
-				Bucket: 'bumblebee', //process.env.DO_BUCKET,
-				// ContentType: media.type,
-				ContentType: 'image/png',
-				ACL: 'public-read',
-				Key,
-				Expires: 60 * 2,
-			});
-			new this.dataSourceModel({
-				name,
-				creator: user.userId,
-				url: url.split('?').slice(0, -1).join('?'),
-			}).save();
-			return url;
-		} catch (error) {
-			console.log(error);
-			throw new BadRequestException(error);
-		}
-	}
+  async getPresignedUrl(user, media): Promise<string> {
+    const name = media.name.split(".").slice(0, -1).join(".");
+    const Key = `${user.username}/${name}-${uuidv4()}.${
+      media.type.split("/")[1]
+    }`;
+    try {
+      const url = await this.s3.getSignedUrlPromise("putObject", {
+        Bucket: process.env.DO_BUCKET,
+        ContentType: media.type,
+        // ContentType: 'image/png',
+        ACL: "public-read",
+        Key,
+        Expires: 60 * 2,
+      });
+      new this.dataSourceModel({
+        name,
+        creator: user.userId,
+        url: url.split("?").slice(0, -1).join("?"),
+      }).save();
+      return url;
+    } catch (error) {
+      console.log(error);
+      throw new BadRequestException(error);
+    }
+  }
 
-	async getDataSources(): Promise<DataSource[]> {
-		const dataSources = await this.dataSourceModel.find().exec();
-		return dataSources;
-	}
+  async getDataSources(): Promise<DataSource[]> {
+    const dataSources = await this.dataSourceModel.find().exec();
+    return dataSources;
+  }
 
-	async findOne(dataSourceId: string): Promise<DataSource> {
-		const dataSource = await this.dataSourceModel.findById(dataSourceId).exec();
-		return dataSource;
-	}
+  async findOne(dataSourceId: string): Promise<DataSource> {
+    const dataSource = await this.dataSourceModel.findById(dataSourceId).exec();
+    return dataSource;
+  }
 
-	async newDataSource(
-		dataSourceCreateDto: any,
-		user: User,
-	): Promise<DataSource> {
-		const dataSource = new this.dataSourceModel({
-			...dataSourceCreateDto,
-			creator: user.id,
-		});
+  async newDataSource(
+    dataSourceCreateDto: any,
+    user: User
+  ): Promise<DataSource> {
+    const dataSource = new this.dataSourceModel({
+      ...dataSourceCreateDto,
+      creator: user.id,
+    });
 
-		return dataSource.save();
-	}
+    return dataSource.save();
+  }
 
-	async updateDataSource(
-		dataSourceId: string,
-		dataSourceUpdateDto: any,
-	): Promise<DataSource> {
-		const dataSource = await this.dataSourceModel.findOneAndUpdate(
-			dataSourceId,
-			dataSourceUpdateDto,
-		);
-		return dataSource;
-	}
+  async updateDataSource(
+    dataSourceId: string,
+    dataSourceUpdateDto: any
+  ): Promise<DataSource> {
+    const dataSource = await this.dataSourceModel.findOneAndUpdate(
+      dataSourceId,
+      dataSourceUpdateDto
+    );
+    return dataSource;
+  }
 
-	async deleteDataSource(dataSourceId: string): Promise<void> {
-		const result = await this.dataSourceModel.deleteOne(dataSourceId);
-	}
+  async deleteDataSource(dataSourceId: string): Promise<void> {
+    const result = await this.dataSourceModel.deleteOne(dataSourceId);
+  }
 }
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,26 +1,46 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-// import { WsAdapter } from '@nestjs/platform-ws';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import axios from "axios";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	const options = new DocumentBuilder()
-		.setTitle('Bumblebee API')
-		.setDescription('The Bumblebee API description endpoints')
-		.setVersion('0.1.0')
-		.build();
-	app.enableCors({
-		origin: [
-			'http://localhost:3000',
-			'https://www.hi-bumblebee.com',
-			'https://app.hi-bumblebee.com',
-		],
-	});
+  const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle("Bumblebee API")
+    .setDescription("The Bumblebee API description endpoints")
+    .setVersion("0.1.0")
+    .build();
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "https://www.hi-bumblebee.com",
+      "https://app.hi-bumblebee.com",
+    ],
+  });
 
-	const document = SwaggerModule.createDocument(app, options);
-	SwaggerModule.setup('api', app, document);
-	await app.listen(process.env.PORT || 4000);
-	// app.useWebSocketAdapter(new WsAdapter(app));
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
+  await app.listen(process.env.PORT || 4000);
+  // app.useWebSocketAdapter(new WsAdapter(app));
+  clearKernels();
 }
+
+async function clearKernels() {
+  const kernelAddress = process.env.KERNEL_ADDRESS.split(",");
+  for (let kernelURL of kernelAddress) {
+    try {
+      const { data } = await axios.get(`http://${kernelURL}/api/kernels`);
+      for (let kernel of data) {
+        try {
+          await axios.delete(`http://${kernelURL}/api/kernels/${kernel.id}`);
+        } catch (error) {
+          console.log("error deleting kernel", error);
+        }
+      }
+    } catch (error) {
+      console.log("error getting kernels from server", error);
+    }
+  }
+}
+
 bootstrap();
