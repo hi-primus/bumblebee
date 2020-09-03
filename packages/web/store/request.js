c997e80750f92411da1ab32792543424c09df919
Luis <luisaguirre027@gmail.com>
2020-09-03 15:41:25 -0400

    default api port changed

--- packages/web/store/request.js
+++ packages/web/store/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.API_URL || 'http://localhost:5000'
+const baseUrl = process.env.API_URL || 'http://localhost:4000'
 
 export const actions = {
 
