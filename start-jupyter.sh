93a36e097b9a1655c454ee20fb37acd76da84745
Luis <luisaguirre027@gmail.com>
2020-09-09 18:54:44 -0400

    Shell script adjusted

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,7 +1,7 @@
 
 cd
-pip install --upgrade --force-reinstall --no-deps git+https://github.com/ironmussa/Optimus.git@develop-3.0
-pip3 install --upgrade --force-reinstall --no-deps git+https://github.com/ironmussa/Optimus.git@develop-3.0
+pip install --upgrade --force-reinstall git+https://github.com/ironmussa/Optimus.git@develop-3.0
+pip3 install --upgrade --force-reinstall git+https://github.com/ironmussa/Optimus.git@develop-3.0
 if [ -z "$address" || "$localhost" = false ]
 then
   jupyter kernelgateway --JupyterWebsocketPersonality.list_kernels=True
