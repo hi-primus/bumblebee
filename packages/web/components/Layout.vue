97a284381364f10e10a0552fce0923c6c6f86f43
Luis <luisaguirre027@gmail.com>
2021-02-17 21:47:13 -0400

    Implement Intercom

--- packages/web/components/Layout.vue
+++ packages/web/components/Layout.vue
@@ -10,6 +10,17 @@
 export default {
 	props: {
 
-	}
+	},
+
+
+  mounted () {
+    if (process.env.INTERCOM_APP_ID) {
+      window.intercomSettings = {
+        app_id: process.env.INTERCOM_APP_ID
+      };
+      // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/process.env.INTERCOM_APP_ID'
+      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/'+process.env.INTERCOM_APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    }
+  }
 }
 </script>
