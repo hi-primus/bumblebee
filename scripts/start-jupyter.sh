3b5133af2ed257911c2518f42fa7427465667948
Luis <luisaguirre027@gmail.com>
2021-06-30 17:17:38 -0400

    Fix start-jupyter script bug
    closes #129

--- scripts/start-jupyter.sh
+++ scripts/start-jupyter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -z "$address" || "$localhost" = false ]; then
+if [[ -z "$address" || "$localhost" = false ]]; then
   jupyter kernelgateway --JupyterWebsocketPersonality.list_kernels=True
 else
   jupyter kernelgateway --ip=0.0.0.0 --JupyterWebsocketPersonality.list_kernels=True --KernelGatewayApp.allow_origin='*' --Application.log_level=50
