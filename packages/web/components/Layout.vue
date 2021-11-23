130be8f033280eba72cb8d95779dc1097ba168cb
Luis <luisaguirre027@gmail.com>
2021-11-23 18:19:32 -0400

    Support Intercom disabling

--- packages/web/components/Layout.vue
+++ packages/web/components/Layout.vue
@@ -12,17 +12,28 @@ import { INTERCOM_APP_ID } from 'bumblebee-utils'
 
 export default {
   mounted () {
-    window.intercomSettings = {
-      app_id: INTERCOM_APP_ID
-    };
-    setTimeout(() => {
-      window.intercomSettings = {
-        app_id: INTERCOM_APP_ID,
-        vertical_padding: 48
-      };
-      window.Intercom("update");
-    }, 150);
-    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/'+INTERCOM_APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    this.enableIntercom()
+  },
+
+  methods: {
+    enableIntercom () {
+      if (INTERCOM_APP_ID && !process.env.DISABLE_INTERCOM) {
+        window.intercomSettings = {
+          app_id: INTERCOM_APP_ID
+        };
+        setTimeout(() => {
+          window.intercomSettings = {
+            app_id: INTERCOM_APP_ID,
+            vertical_padding: 48
+          };
+          window.Intercom("update");
+        }, 150);
+        (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/'+INTERCOM_APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+      } else {
+        window.Intercom = ()=>{};
+        window.intercomSettings = {};
+      }
+    }
   }
 }
 </script>
