564d1f57639cf2dbf94312ecff4c5597b4d5b128
Luis Aguirre <luisaguirre027@gmail.com>
2022-03-01 14:40:22 -0400

    Bump version

--- scripts/update-bumblebee.bat
+++ scripts/update-bumblebee.bat
@@ -1,6 +1,6 @@
 cd ..
 
-git reset --hard origin/develop-22.2
+git reset --hard origin/develop-22.3
 git pull
 
 cd scripts
--- scripts/update-bumblebee.sh
+++ scripts/update-bumblebee.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 
-git reset --hard origin/develop-22.2
+git reset --hard origin/develop-22.3
 git pull
 
 cd scripts
