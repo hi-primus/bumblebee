5226d21f3b5d7c8c7c6c703775aa604d00013415
Luis Aguirre <luisaguirre027@gmail.com>
2022-03-24 12:47:48 -0400

    Fix concat bugs

--- packages/web/components/DraggableConcat.vue
+++ packages/web/components/DraggableConcat.vue
@@ -70,7 +70,7 @@
           <v-icon
             small
             class="close-button"
-            @click="removeOutputItem(index)"
+            @click="removeRow(index)"
           >
             close
           </v-icon>
@@ -124,7 +124,7 @@
 
 <script>
 
-import { propsToLocal, debounce, transpose } from 'bumblebee-utils'
+import { propsToLocal, deepCopy, incrementVarName, debounce, transpose } from 'bumblebee-utils'
 import SearchSelect from '@/components/SearchSelect'
 
 export default {
@@ -154,7 +154,7 @@ export default {
       searchPromise: false,
       searchSelectAttach: false,
       searchItems: [],
-      itemsLength: 0,
+      itemsLength: -1,
       itemsSlotsGroups: [],
       notSelected: [],
       textFields: [],
@@ -209,7 +209,7 @@ export default {
       if (!this.notSelected) {
         return true
       }
-      return !(this.notSelected || [[]]).some(e=>e.length)
+      return !(this.notSelected || [[]]).some(e=>e && e.length)
     },
 
     ...propsToLocal(['items', 'selected']),
@@ -217,7 +217,7 @@ export default {
   },
 
   mounted () {
-    this.updateItemsSlotsGroups();
+    this.resetItemsSlotsGroups(false);
   },
 
   watch: {
@@ -226,8 +226,9 @@ export default {
       deep: true,
       handler (items) {
         if (items.length!==this.itemsLength) {
+          let reset = this.itemsLength > 0;
           this.itemsLength = items.length;
-          this.updateItemsSlotsGroups();
+          this.resetItemsSlotsGroups(reset);
         }
       }
     },
@@ -269,43 +270,7 @@ export default {
     },
 
     localSelected () {
-
-      var defaultValues = []
-      this.localSelected.forEach(e=>{
-        var name = 'error'
-
-        if (e.items && e.items.length) {
-
-          var items = e.items.filter(ee=>ee).map(ee=>ee[this.itemsKey] ? ee[this.itemsKey] : ee)
-          if (items.every(ee=>ee==items[0])) {
-            name = items[0]
-          } else {
-            name = items.join('_')
-          }
-
-        }
-
-        while (defaultValues.includes(name)) {
-          name = name+' copy'
-        }
-
-        defaultValues.push(name)
-
-      });
-
-      var textFieldsValues = {};
-
-      defaultValues.forEach(name=>{
-        if (name) {
-          textFieldsValues[name] = this.textFieldsValues[name] || ''
-        }
-      })
-
-      if (JSON.stringify(this.textFieldsValues)!==JSON.stringify(textFieldsValues)) {
-        this.textFieldsValues = textFieldsValues;
-      }
-
-      this.textFields = defaultValues
+      this.updateTextFields()
     },
 
 
@@ -400,38 +365,108 @@ export default {
       return true;
     },
 
-    removeOutputItem (slotIndex) {
+    removeRow (rowIndex) {
 
-      var itemsSlotsGroups = Array.from(this.itemsSlotsGroups);
+      this.$delete(this.textFieldsValues, this.textFields[rowIndex]);
 
-      this.notSelected.forEach((notSelectedGroup, groupIndex) => {
-        this.itemsSlotsGroups[groupIndex][slotIndex].forEach(e=>{
-          this.notSelected[groupIndex].push(e);
+      this.$delete(this.textFields, rowIndex);
+
+      let itemsSlotsGroups = Array.from(this.itemsSlotsGroups);
+
+      this.notSelected.forEach((notSelectedGroup, colIndex) => {
+        this.itemsSlotsGroups[colIndex][rowIndex].forEach(e=>{
+          this.notSelected[colIndex].push(e);
         })
-        itemsSlotsGroups[groupIndex][slotIndex] = []
+        itemsSlotsGroups[colIndex][rowIndex] = []
         return true;
       })
 
-      var deleteEmptyResults = this.deleteEmptyItemsSlots(itemsSlotsGroups);
+      let deleteEmptyResults = this.deleteEmptyItemsSlots(itemsSlotsGroups);
       if (deleteEmptyResults) {
         itemsSlotsGroups = deleteEmptyResults;
       }
       this.itemsSlotsGroups = itemsSlotsGroups;
+
     },
 
-    updateItemsSlotsGroups () {
+    updateTextFields () {
+      try {
+        let defaultValues = [];
+        let textFieldsValues = {};
+  
+        this.localSelected.forEach(row=>{
+          let name = 'error';
+  
+          if (row.items && row.items.length) {
+  
+            let items = row.items.filter(col=>col).map(col=>col[this.itemsKey] ? col[this.itemsKey] : col)
+            if (items.every(col=>col==items[0])) {
+              name = items[0];
+            } else {
+              name = items.join('_');
+            }
+  
+          }
+  
+          let fieldName = incrementVarName(name, defaultValues);
+          name = incrementVarName(name, Object.values(textFieldsValues));
+  
+          defaultValues.push(fieldName);
+          textFieldsValues[fieldName] = row.value || this.textFieldsValues[fieldName] || name || '';
+  
+        });
+  
+        if (JSON.stringify(this.textFieldsValues)!==JSON.stringify(textFieldsValues)) {
+          this.textFieldsValues = textFieldsValues;
+        }
+  
+        this.textFields = defaultValues;
 
-      var length = Math.max(...this.items.map(eg=>eg.length))+1;
+      } catch (err) {
+        console.error(err);
+      }
 
-      this.itemsSlotsGroups = this.items.map(eg=>{
-        var array = eg ? Array.from(eg) : [];
-        var al = array.length
-        array.length = length
-        array.fill(false, al)
-        return array.map(e=>(e ? [e] : []))
-      });
+    },
 
-      this.notSelected = this.items.map(eg=>[]);
+    resetItemsSlotsGroups (reset = false) {
+
+      try {
+
+        let length = Math.max(...this.items.map(col=>col.length))+1;
+        let itemsSlotsGroups;
+
+        if (reset || !this.selected.length) {
+          itemsSlotsGroups = this.items;
+        } else {
+          itemsSlotsGroups = transpose(this.selected.map(row => row.items));
+        }
+
+        this.itemsSlotsGroups = itemsSlotsGroups.map(col => {
+          let array = col ? Array.from(col) : [];
+          let al = array.length
+          array.length = length
+          array.fill(false, al)
+          return array.map(e=>(e ? [e] : []))
+        });
+
+        if (reset || !this.selected.length) {
+          this.notSelected = this.items.map(col=>[]);
+        } else {
+          this.updateTextFields();
+          this.notSelected = this.items.map((col, colIndex) => {
+            let array = [];
+            col.forEach(e => {
+              if (e && e.name && !this.itemsSlotsGroups[colIndex].find(ie => ie && ie.length && ie[0].name === e.name)) {
+                array.push(e);
+              }
+            });
+            return array;
+          });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+
     },
 
     updateTextField (field, value) {
@@ -449,11 +484,21 @@ export default {
       })
 
       var rows = transpose(columns).map((rowItems, i)=>{
+        let items = rowItems.map(it=>it!=='_BB_EMPTY_SLOT_' ? it : false);
+        let firstItemName = items.find(it=>it && it.name).name;
+
         return {
-          items: rowItems.map(it=>it!=='_BB_EMPTY_SLOT_' ? it : false),
-          value: this.textFieldsValues[this.textFields[i]] || this.textFields[i] || 'und'
+          items,
+          value: this.textFieldsValues[this.textFields[i]] || this.textFields[i] || firstItemName || ''
         }
-      })
+      });
+
+      rows = rows.map((row, rowIndex) => {
+        let previousRow = rows.slice(0, rowIndex);
+        let previousRowValues = previousRow.map(r=>r.value);
+        row.value = incrementVarName(row.value, previousRowValues);
+        return row;
+      });
 
       this.localSelected = rows
     },
