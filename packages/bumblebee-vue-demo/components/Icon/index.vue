b4a6320b0723672c2544e65ed0cda17902acadcd
Luis Aguirre <luisaguirre027@gmail.com>
2022-11-03 15:34:54 -0400

    Include mdi icon support

---
+++ packages/bumblebee-vue-demo/components/Icon/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg class="w-5 h-5 text-text" viewBox="0 0 24 24" style="fill: currentColor;">
+    <path :d="path" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+  const { path } = defineProps({
+    path: {
+      type: String,
+      required: true,
+    }
+  });
+</script>
