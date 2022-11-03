7290951183807d1256143e7ec8d36253cdabd628
Luis Aguirre <luisaguirre027@gmail.com>
2022-11-03 15:33:20 -0400

    Include colors on tailwind config

--- packages/bumblebee-vue-demo/tailwind.config.js
+++ packages/bumblebee-vue-demo/tailwind.config.js
@@ -8,7 +8,44 @@ module.exports = {
     './nuxt.config.{js,ts}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        900: '#205a98',
+        dark: '#277abb',
+        800: '#277abb',
+        700: '#2b8bcf',
+        DEFAULT: '#309ee3',
+        600: '#309ee3',
+        500: '#34acf2',
+        400: '#42b9f4',
+        300: '#5dc6f6',
+        light: '#88d6fa',
+        200: '#88d6fa',
+        100: '#b6e6fc',
+        50: '#e2f5fe'
+      },
+      error: {
+        dark: '#d53134',
+        DEFAULT: '#f15555',
+        light: '#f99290',
+      },
+      warn: {
+        dark: '#e0a019',
+        DEFAULT: '#e3ce30',
+        light: '#f0ef9a',
+      },
+      text: {
+        DEFAULT: '#24282e',
+        light: '#656970',
+        lighter: '#a3a7af',
+      },
+    },
+    extend: {
+      fontFamily: {
+        'mono': ['Ubuntu Mono', 'monospace'],
+        'mono-table': ['Ubuntu Mono Table', 'Ubuntu Mono', 'monospace']
+      },
+    },
   },
   plugins: [],
 }
