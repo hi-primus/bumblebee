3c74f96885bde6abe14dd6a2dccac16f4956c797
Luis Aguirre <luisaguirre027@gmail.com>
2022-11-04 14:34:47 -0400

    Include sidebar header

--- packages/bumblebee-vue-demo/components/Workspace/Operations.vue
+++ packages/bumblebee-vue-demo/components/Workspace/Operations.vue
@@ -1,5 +1,26 @@
 <template>
-  <aside>
+  <aside
+    class="
+      workspace-aside
+      bg-white
+      border-line
+      border-solid
+      border-l
+    "
+  >
+    <div
+      class="
+        w-full
+        h-8
+        bg-primary-50
+        text-text/90
+        flex
+        justify-center
+        items-center
+      "
+    >
+      <h3>Operations</h3>
+    </div>
     <ul class="p-4">
       <OperationsOperation
         v-for="(operation, index) in operations"
