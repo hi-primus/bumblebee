d5784e5b100752d6ed64a6ef6cc759f91867139f
Eddy Aguirre <eddyj.aguirre@gmail.com>
2022-11-04 18:36:27 -0400

    Change footer

--- packages/bumblebee-vue-demo/components/Workspace/Footer.vue
+++ packages/bumblebee-vue-demo/components/Workspace/Footer.vue
@@ -1,3 +1,33 @@
 <template>
-  <footer class="workspace-footer bg-white border-line border-solid border-t"></footer>
+  <footer
+    class="workspace-footer bg-white border-line border-solid border-t"
+  >
+    <div class="flex h-full justify-end items-center gap-8 mx-8 font-mono">
+      <span>{{ props.dataTypes }} Data types</span>
+      <span>{{ props.rowsLoaded }} of {{ props.rowsTotal }} rows</span>
+      <span>{{ props.columns }} columns</span>
+    </div>
+  </footer>
 </template>
+<script setup lang="ts">
+
+const props = defineProps({
+  columns: {
+    type: Number,
+    default: 0
+  },
+  rowsLoaded: {
+    type: Number,
+    default: 0
+  },
+  rowsTotal: {
+    type: Number,
+    default: 0
+  },
+  dataTypes: {
+    type: Number,
+    default: 0
+  },
+});
+
+</script>
