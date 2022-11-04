3db9bbc45a29743e9e4e15b268374466d8999393
Eddy Aguirre <eddyj.aguirre@gmail.com>
2022-11-04 10:23:39 -0400

    Auto stash before merge of "main" and "origin/main"

---
+++ packages/bumblebee-vue-demo/components/Operations/Operation.vue
@@ -0,0 +1,13 @@
+<template>
+  <li
+    class="rounded-md border-text-lighter border border-l-8 border border-text"
+  >
+    <textarea
+      name=""
+      id=""
+      rows="3"
+      class="w-full focus:outline-none"
+    >
+    </textarea>
+  </li>
+</template>
--- packages/bumblebee-vue-demo/components/Workspace/Operations.vue
+++ packages/bumblebee-vue-demo/components/Workspace/Operations.vue
@@ -1,4 +1,15 @@
 <template>
-  <aside class="bg-white border-line border-solid border-l">
+  <aside>
+    <ul class="p-4">
+      <OperationsOperation
+        v-for="(operation, index) in operations"
+        :key="index"
+        :class="{'mt-2': index !== 0}"
+      />
+    </ul>
   </aside>
 </template>
+
+<script setup lang="ts">
+let operations: Array<Object> = [{}, {}, {}, {}]
+</script>
