112b15afd34f82e95789f33de44d380f8f66e3f5
Argenis Leon <argenisleon@gmail.com>
2022-11-15 18:25:54 -0600

    Add rows function to dataframe

--- packages/blurr/src/lib/client/operations/dataframe/index.ts
+++ packages/blurr/src/lib/client/operations/dataframe/index.ts
@@ -1,5 +1,33 @@
+import type {
+  CallbackFunction,
+  Cols,
+  NoArgs,
+  SearchBy,
+  Source,
+} from '../../../../types/arguments';
+import { ArgsType, OperationCreator } from '../../../../types/operation';
+import { BlurrOperation } from '../factory';
+
 import { operations as colsOperations } from './cols';
+import { operations as rowsOperations } from './rows';
+
+function DataframeOperation<TA extends ArgsType = ArgsType,
+  TR extends OperationCompatible = Source>(operationCreator: OperationCreator) {
+  return BlurrOperation<TA, TR>({
+    ...operationCreator,
+    sourceType: 'dataframe',
+  });
+}
 
 export const operations = {
   ...colsOperations,
+  ...rowsOperations,
+  columnsSample: DataframeOperation<NoArgs, Source>({
+    name: 'columnsSample',
+    args: [],
+    defaultSource: 'df',
+
+  }
 };
+
+
