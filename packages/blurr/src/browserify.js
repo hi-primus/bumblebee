83cdd0ae95d35d59973fcd1f2b5fa1f5dd82afac
Luis Aguirre <luisaguirre027@gmail.com>
2022-11-21 14:36:28 -0400

    Include browserify file on tsconfig

--- packages/blurr/src/browserify.js
+++ packages/blurr/src/browserify.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const blurr = require('../build/main/index.js');
 global.window.blurr = { ...blurr };
 module.exports = { ...blurr };
