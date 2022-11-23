01e5bc77a757e99feee38fa4d24ac1a920c86cd3
Eddy Aguirre <eddyj.aguirre@gmail.com>
2022-11-23 11:36:05 -0400

    Add Popup

---
+++ packages/bumblebee-vue-demo/components/App/Popup.vue
@@ -0,0 +1,34 @@
+<template>
+  <Teleport to="body">
+    <div
+      class="w-full h-screen absolute top-0 left-0 z-10 bg-black/80 flex items-center justify-center"
+    >
+      <div
+        class="relative bg-gray-50 rounded-lg w-120 p-5 border-2 border-black max-h-[calc(100vh-5rem)]"
+        :class="{
+          'pt-8': closable
+        }"
+      >
+        <AppButton
+          v-if="closable"
+          class="absolute right-3 top-3"
+          variant="roundedIcon"
+          @click="emit('close')"
+        >
+          <font-awesome-icon icon="close" />
+        </AppButton>
+        <slot :close="() => emit('close')"> </slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  closable: {
+    type: Boolean,
+    default: true
+  }
+});
+const emit = defineEmits(['close']);
+</script>
