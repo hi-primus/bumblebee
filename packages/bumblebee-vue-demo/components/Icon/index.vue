93428e31348b6d5438c4dfed585682e1016f3bd6
Luis Aguirre <luisaguirre027@gmail.com>
2022-12-05 10:24:24 -0400

    Include class to Icon component

--- packages/bumblebee-vue-demo/components/Icon/index.vue
+++ packages/bumblebee-vue-demo/components/Icon/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <svg width="24" height="24" viewBox="0 0 24 24" style="fill: currentColor">
-    <path :d="path" />
+  <svg class="mdi-icon" width="24" height="24" viewBox="0 0 24 24" style="fill: currentColor">
+    <path v-if="path" :d="path" />
   </svg>
 </template>
 
 <script setup lang="ts">
+import { PropType } from 'vue';
+
 const { path } = defineProps({
   path: {
-    type: String,
-    required: true
+    type: [String, Boolean] as PropType<string | false>,
   }
 });
 </script>
