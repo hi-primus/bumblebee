2fbd987758f9139cc8c28a8a959513e55b42a57a
Luis Aguirre <luisaguirre027@gmail.com>
2022-12-16 21:33:45 -0400

    Use global scope in any import

--- packages/blurr/src/browserify.js
+++ packages/blurr/src/browserify.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const blurr = require('../build/main/index.js');
-global.window.blurr = { ...blurr };
 module.exports = { ...blurr };
--- packages/blurr/src/index.ts
+++ packages/blurr/src/index.ts
@@ -1,3 +1,11 @@
-export * from './lib/client';
-export * from './lib/client/data/source';
-export * from './lib/server';
+import { Blurr as _Blurr } from './lib/client';
+import { Source as _Source } from './lib/client/data/source';
+import { Server as _Server } from './lib/server';
+
+export const Blurr = _Blurr;
+export const Source = _Source;
+export const Server = _Server;
+
+globalThis.blurr = { Blurr, Source, Server };
+
+export default { Blurr, Source, Server };
