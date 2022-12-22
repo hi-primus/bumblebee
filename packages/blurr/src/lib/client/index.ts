ec3d19673f28c50697986094f04b3c239fea4fa9
Luis Aguirre <luisaguirre027@gmail.com>
2022-12-22 17:31:58 -0400

    Remove new name generation for every operation on remote processing

--- packages/blurr/src/lib/client/index.ts
+++ packages/blurr/src/lib/client/index.ts
@@ -96,9 +96,9 @@ export function Blurr(options: ClientOptions = {}): Client {
     if (lastOperation && lastOperation.targetType === 'dataframe') {
       const lastParams = paramsQueue[paramsQueue.length - 1];
       if (!('target' in lastParams)) {
-        lastParams.target = generateUniqueVariableName(
-          lastOperation.targetType
-        );
+        lastParams.target =
+          lastParams.source ||
+          generateUniqueVariableName(lastOperation.targetType);
       }
 
       if (!blurr.options.serverOptions.local) {
