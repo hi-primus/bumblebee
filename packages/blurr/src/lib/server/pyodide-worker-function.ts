d175745e90c1c82eb83f26d9d809f45ec2a4fda8
Luis Aguirre <luisaguirre027@gmail.com>
2023-01-11 18:47:41 -0400

    Fix worker bug on builds

---
+++ packages/blurr/src/lib/server/pyodide-worker-function.ts
@@ -0,0 +1,140 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare const self, importScripts, loadPyodide;
+
+export const initializeWorker = () => {
+  // TODO: import fetch, allow scriptURL?, handle Source objects
+  importScripts('https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js');
+
+  let backendLoaded = false;
+
+  function _mapToObject(map) {
+    const obj = {};
+    for (const [key, value] of map) {
+      obj[key] = value;
+    }
+    return obj;
+  }
+
+  async function initialize(options = {}) {
+    if (!backendLoaded) {
+      options = Object.assign(
+        { indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.21.3/full/' },
+        options
+      );
+
+      self.pyodide = await loadPyodide(options);
+
+      await self.pyodide.loadPackage('micropip');
+      self.micropip = self.pyodide.pyimport('micropip');
+
+      await self.micropip.install(
+        'https://test-files.pythonhosted.org/packages/4b/41/a3e8331bd09ffcca4834715b4c269fc2cfa5acdfa4fb080f02a3c0acf688/pyoptimus-0.1.40212-py3-none-any.whl'
+      );
+      self.pyodide.runPython(`
+        from optimus import Optimus
+        from io import BytesIO
+        op = Optimus("pyscript")
+        def run_method(method, kwargs):
+            return method(**kwargs.to_py())
+      `);
+
+      backendLoaded = true;
+    }
+  }
+
+  let initialization;
+
+  addEventListener(
+    'message',
+    async (e) => {
+      if (typeof e.data !== 'object' || !('type' in e.data)) {
+        throw new Error('Invalid message');
+      }
+
+      if (e.data.type === 'init') {
+        initialization = initialize(e.data.options);
+
+        await initialization;
+
+        self.postMessage({
+          type: 'init',
+          id: e.data.id,
+        });
+      } else if (e.data.type === 'load') {
+        await initialization;
+
+        if (!backendLoaded) {
+          console.warn('Backend not loaded, loading with default options...');
+          await initialize();
+        }
+
+        if (e.data.packages && e.data.packages.length > 0) {
+          await self.pyodide.loadPackage(e.data.packages);
+        }
+
+        self.postMessage({
+          type: 'load',
+          id: e.data.id,
+        });
+      } else if (e.data.type === 'run') {
+        await initialization;
+
+        if (!backendLoaded) {
+          console.warn('Backend not loaded, loading with default options...');
+          await initialize();
+        }
+
+        let result = null;
+        let error = null;
+
+        try {
+          if (e.data.code) {
+            result = self.pyodide.runPython(e.data.code);
+          } else {
+            const runMethod = self.pyodide.globals.get('run_method');
+            result = runMethod(e.data.method, e.data.kwargs);
+          }
+
+          try {
+            result =
+              typeof result?.toJs === 'function'
+                ? result.toJs({ dict_converter: _mapToObject })
+                : result;
+          } catch (error) {
+            console.warn('Error converting to JS', error);
+          }
+        } catch (err) {
+          error = err.message;
+        }
+
+        try {
+          self.postMessage({
+            type: 'run',
+            id: e.data.id,
+            result,
+            error,
+          });
+        } catch (err) {
+          if (err.message.includes('could not be cloned')) {
+            const source = {} as any;
+            source.name = e.data.kwargs?.target;
+            source._blurrMember = 'source';
+            self.postMessage({
+              type: 'run',
+              id: e.data.id,
+              result: source,
+            });
+          } else {
+            self.postMessage({
+              type: 'run',
+              id: e.data.id,
+              result: null,
+              error: err.message,
+            });
+          }
+        }
+      }
+    },
+    false
+  );
+};
