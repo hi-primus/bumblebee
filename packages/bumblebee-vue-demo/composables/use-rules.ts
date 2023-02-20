22cbaeefc7055f73cc01b2a780986e62a2755350
Argenis Leon <argenisleon@gmail.com>
2023-02-20 10:49:58 -0600

    Missing ;

--- packages/bumblebee-vue-demo/composables/use-rules.ts
+++ packages/bumblebee-vue-demo/composables/use-rules.ts
@@ -1,9 +1,9 @@
 const ruleMethods = {
   required: (value: any) => !!value || 'Required.',
-}
+};
 
 export type RuleKey = keyof typeof ruleMethods
 
 export default function (rules: RuleKey[]) {
-  return rules.map((rule) => ruleMethods[rule]);
+  return rules.map(rule => ruleMethods[rule]);
 }
