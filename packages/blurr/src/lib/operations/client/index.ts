67ccdfd613fe1e4dfb12698ce15d516167e31393
Luis Aguirre <luisaguirre027@gmail.com>
2023-02-21 16:46:27 -0400

    Sort dataframe creation operations

--- packages/blurr/src/lib/operations/client/index.ts
+++ packages/blurr/src/lib/operations/client/index.ts
@@ -14,6 +14,13 @@ function DataframeCreationOperation<
 }
 
 export const operations = {
+  createDataframe: DataframeCreationOperation<{
+    data: Record<string, PythonCompatible>;
+  }>({
+    name: 'create.dataframe',
+    defaultSource: 'op',
+    args: [{ name: 'data' }],
+  }),
   readCsv: DataframeCreationOperation<{
     url?: string;
     buffer?: string;
@@ -54,13 +61,6 @@ export const operations = {
       }
       throw new Error("Either 'url' or 'buffer' must be provided");
     },
-  }),
-  createDataframe: DataframeCreationOperation<{
-    data: Record<string, PythonCompatible>;
-  }>({
-    name: 'create.dataframe',
-    defaultSource: 'op',
-    args: [{ name: 'data' }],
   }),
   readFile: DataframeCreationOperation<{
     url?: string;
