33f1b4c89fe94629bf0b87c45a7d1dc58ec9c7ce
Luis Aguirre <luisaguirre027@gmail.com>
2023-03-01 15:44:54 -0400

    Include types on blurr export

--- packages/blurr/src/index.ts
+++ packages/blurr/src/index.ts
@@ -2,9 +2,24 @@ import { Blurr as _Blurr } from './lib/client';
 import { Source as _Source } from './lib/client/data/source';
 import { Server as _Server } from './lib/server';
 
+export { ClientOptions } from './types/client';
+export { ServerOptions } from './types/server';
+export { Client as ClientInterface } from './types/client';
+export { Server as ServerInterface } from './types/server';
+export { Source as SourceInterface } from './types/source';
+
 export const Blurr = _Blurr;
 export const Source = _Source;
 export const Server = _Server;
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace globalThis {
+  let blurr: {
+    Blurr: typeof _Blurr;
+    Source: typeof _Source;
+    Server: typeof _Server;
+  };
+}
 
 globalThis.blurr = { Blurr, Source, Server };
 
