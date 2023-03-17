32d021da9e643778255b44b62461e408dc261003
Luis Aguirre <luisaguirre027@gmail.com>
2023-03-17 12:31:44 -0400

    Create IconButton component

---
+++ packages/bumblebee-vue-demo/components/Icon/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="foucs:outline-1 outline-[currentColor]" type="button">
+    <Icon class="h-[inherit] w-[inherit]" role :path="path" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+defineProps({
+  path: {
+    type: [String, Boolean] as PropType<string | false>
+  }
+});
+</script>
